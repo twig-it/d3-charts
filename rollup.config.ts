f8fa2f904c9f00e87a94f954bc3a9f86295cb730
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-03-21 14:39:51 -0700

    refactor: updating build configs

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,7 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
 export default {
-  input: 'src/public-api.ts',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
