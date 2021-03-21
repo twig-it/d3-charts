5de6a91031cc40de57ee510601341576d48c6fb2
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-03-21 16:25:34 -0700

    refactor: updating build config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,31 +1,42 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: [
     {
       file: pkg.main,
       name: pkg.name,
-      format: 'umd',
+      format: "umd",
       sourcemap: true,
       globals: {
-        'lodash-es': '_'
-      }
+        "lodash-es": "_",
+      },
     },
-    { file: pkg.module, format: 'es', sourcemap: true }
+    { file: pkg.module, format: "es", sourcemap: true },
   ],
-  external: ['lodash-es'],
+  external: [
+    "lodash-es",
+    "d3-array",
+    "d3-axis",
+    "d3-color",
+    "d3-format",
+    "d3-scale",
+    "d3-selection",
+    "d3-shape",
+    "d3-time-format",
+    "d3-transition",
+  ],
   watch: {
-    include: 'src/**'
+    include: "src/**",
   },
   plugins: [
     typescript({
-      exclude: ['*.d.ts', '**/*.d.ts', '**/test/**', '**/*.test.ts']
+      exclude: ["*.d.ts", "**/*.d.ts", "**/test/**", "**/*.test.ts"],
     }),
     commonjs(),
-    resolve()
-  ]
+    resolve(),
+  ],
 };
